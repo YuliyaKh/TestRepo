1e4e118a70465bb8b81206e46af5312082f44fe5
YuliyaKhoptyan <kh.yulia.o@gmail.com>
2015-04-18 23:32:43 +0600

    Init commit

---
+++ Client/ClientHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class ClientHandler
+    {
+       
+        public ClientHandler(IPAddress ipAddress)
+        {
+           
+            var massive = new Int32[3];
+            int port = 80;            
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+            
+            var client = new TcpClient();
+            client.Connect(ipEndPoint);
+            Console.WriteLine("Enter three numbers");
+            byte[] buffer = new byte[12];
+           
+            int value;
+            
+            for (int i = 0; i < 3; i++)
+            {
+                while (!int.TryParse(Console.ReadLine(), out value))
+                {
+                    Console.WriteLine("Value cannot be letter! Enter the number.");
+                }
+                massive[i] = value;                    
+                
+                var data = BitConverter.GetBytes(massive[i]);
+                data.CopyTo(buffer, i * 4);
+            }
+            client.GetStream().Write(buffer, 0, buffer.Length);
+
+            client.Close();
+        }
+
+        
+    }
+}
---
+++ Server/ServerHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client_Server
+{
+    class ServerHandler
+    {
+        int sum = 0;
+        public static ManualResetEvent tcpClientConnected = new ManualResetEvent(false);
+        public ServerHandler(int port)
+        {
+            var listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+
+
+            while (true)
+            {
+                tcpClientConnected.Reset();
+                listener.BeginAcceptTcpClient(AcceptClient, listener);
+                tcpClientConnected.WaitOne();
+            }
+        }
+
+        private void AcceptClient(IAsyncResult ar)
+        {
+            var listener = ar.AsyncState as TcpListener;
+            var client = listener.EndAcceptTcpClient(ar);
+            tcpClientConnected.Set();
+            HandleRequest(client);
+        }
+
+        public void HandleRequest(TcpClient client)
+        {
+            int[] mass = new int[3];
+            byte[] buffer = new byte[100];
+
+            try
+            {
+                client.GetStream().Read(buffer, 0, buffer.Length);
+            }
+            catch
+            {
+                client.Close();
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                mass[i] = BitConverter.ToInt32(buffer, i * 4);
+                sum = sum + mass[i];
+            }
+            Console.WriteLine("Total: {0}", sum);
+            client.Close();
+        }
+    }
+}
